a9e6bebeba14f9206d2eb6f5ea149511d8b0d558
pratyushved <pratyush.singh@myccmg.com>
2024-05-15 11:09:24 +0530

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,6 +1,6 @@
-def calculation(expression):
-    def apply_operator(operators, values):
-        operator = operators.pop()
+class Calculator:
+    @staticmethod
+    def apply_operator(operators, values, operator):
         right = values.pop()
         left = values.pop()
         if operator == '+':
@@ -12,32 +12,34 @@ def calculation(expression):
         elif operator == '/':
             values.append(left / right)
 
+    @staticmethod
     def greater_precedence(op1, op2):
         precedences = {'+': 1, '-': 1, '/': 2, '*': 2}
         return precedences[op1] > precedences[op2]
 
-    operators = []
-    values = []
-    i = 0
-    expression = expression.replace(" ", "")
-    while i < len(expression):
-        if expression[i] in '0123456789':
-            j = i
-            while j < len(expression) and expression[j] in '0123456789':
-                j += 1
-            values.append(int(expression[i:j]))
-            i = j
-        else:
-            while operators and greater_precedence(operators[-1], expression[i]):
-                apply_operator(operators, values)
-            operators.append(expression[i])
-            i += 1
+    def calculate(self, expression):
+        operators = []
+        values = []
+        expression = expression.replace(" ", "")
+        i = 0
+        while i < len(expression):
+            if expression[i] in '0123456789':
+                j = i
+                while j < len(expression) and expression[j] in '0123456789':
+                    j += 1
+                values.append(int(expression[i:j]))
+                i = j
+            else:
+                while operators and Calculator.greater_precedence(operators[-1], expression[i]):
+                    Calculator.apply_operator(operators, values, operators.pop())
+                operators.append(expression[i])
+                i += 1
 
-    while operators:
-        apply_operator(operators, values)
-    return values[0]
+        while operators:
+            Calculator.apply_operator(operators, values, operators.pop())
+        return values[0]
 
 
 expression = " 3 * 4 - 2"
-result = calculation(expression)
+result = Calculator().calculate(expression)
 print(result)
